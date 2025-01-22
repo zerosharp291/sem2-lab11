a750de4285a186b531ea7b9c46aff4459104d480
Zufar Sharipov <zerosharp291@gmail.com>
2025-01-22 16:32:24 +0400

    Refactored task C

--- src/codeforce_C.cpp
+++ src/codeforce_C.cpp
@@ -2,78 +2,82 @@
 #include <vector>
 using namespace std;
 
-int main() {
-    long long n; // кол-во товаров на кассе
-    cin >> n;
-    long long s;
-    cin >> s; // кол-во наличных деняг
+int main()
+{
+    long long products_count; // кол-во товаров на кассе
+    cin >> products_count;
+    long long cash_count;
+    cin >> cash_count; // кол-во наличных
 
     vector<long long> price; // цена каждого товара
     vector<long long> value; // важность каждого товара
 
-    // ввод цены и важности каждого товара 
-    for (long long i = 0;  i < n; i++) {
-        long long x;
-        cin >> x;
-        price.push_back(x);
-        long long y;
-        cin >> y;
-        value.push_back(y);
+    long long cur_price;
+    long long cur_value;
+    // ввод цены и важности каждого товара
+    for (long long i = 0; i < products_count; i++)
+    {
+        cin >> cur_price;
+        price.push_back(cur_price);
+        cin >> cur_value;
+        value.push_back(cur_value);
     }
 
-    long long current_price = 0; // текущая цена
-    long long current_value = 0; // текущая важность
-    //long long total_price = 0; // итоговая цена
+    // long long total_price = 0; // итоговая цена
     long long total_value = 0; // итоговая важность
 
     long long first_item = 0; // первый товар
-    long long last_item = 0; // последний товар
-    long long current_first_item = 0;
-    long long current_last_item = 0;
+    long long last_item = 0;  // последний товар
+    long long cur_first_item = 0;
+    long long cur_last_item = 0;
 
-    current_price += price[0];
-    current_value += value[0];
+    cur_price = price[0]; // текущая цена
+    cur_value = value[0]; // текущая важность
 
     bool flag = false;
 
-    for(long long i = 0; i < n; i++){
-        //cout << "ALARM";
-        if (price[i] <= s){
+    // Проверка, если ли хотя бы один товар который возможно купить
+    for (long long i = 0; i < products_count; i++)
+    {
+        if (price[i] <= cash_count)
+        {
             flag = true;
             break;
         }
     }
-    if (flag == true){
-          while (last_item < n) {
-            current_last_item++;
-            if (current_last_item >= n){
-                break;
+
+    if (flag == true)
+    {
+        while (last_item < products_count)
+        {
+            cur_last_item++;
+            if (cur_last_item >= products_count)
+            {
+                break; // перебрали всю ленту
             }
-            current_price += price[current_last_item];
-            current_value += value[current_last_item];
-            while (current_price > s && current_first_item < current_last_item){
-                current_price -= price[current_first_item];
-                current_value -= value[current_first_item];
-                current_first_item++;
+
+            cur_price += price[cur_last_item];
+            cur_value += value[cur_last_item];
+
+            while (cur_price > cash_count && cur_first_item < cur_last_item)
+            {
+                cur_price -= price[cur_first_item];
+                cur_value -= value[cur_first_item];
+                cur_first_item++;
             }
-            if (total_value < current_value){
-                total_value = current_value;
-                first_item = current_first_item;
-                last_item = current_last_item;
+            if (total_value < cur_value)
+            {
+                total_value = cur_value;
+                first_item = cur_first_item;
+                last_item = cur_last_item;
             }
         }
 
         cout << first_item + 1 << " ";
         cout << last_item + 1;
     }
-    else {
+    else
+    {
         cout << 0 << " " << 0;
     }
-
-    
-
-    //cout << "total_value = " << total_value << endl;
-    //cout << "current_price = " << current_price << endl;
-
-    
 }
